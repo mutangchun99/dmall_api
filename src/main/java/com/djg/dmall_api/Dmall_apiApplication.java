d741ee970c268004929583a27ae33ada0889db12
djg <mutangchun_dong@163.com>
2022-02-18 17:33:32 +0800

    create project

---
+++ src/main/java/com/djg/dmall_api/Dmall_apiApplication.java
@@ -0,0 +1,13 @@
+package com.djg.dmall_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dmall_apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dmall_apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djg/dmall_api/Dmall_apiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djg.dmall_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dmall_apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
