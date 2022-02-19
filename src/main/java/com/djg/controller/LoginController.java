1585e4fc655cc5796bb09323e9390bb57395cff2
djg <mutangchun_dong@163.com>
2022-02-19 13:36:28 +0800

    UPDATE

---
+++ src/main/java/com/djg/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.djg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class LoginController {
+    @RequestMapping("/login")
+    public String login(){
+        return "hello world";
+    };
+
+}
--- src/main/java/com/djg/dmall_api/Dmall_apiApplication.java
+++ src/main/java/com/djg/dmall_api/Dmall_apiApplication.java
@@ -3,7 +3,7 @@ package com.djg.dmall_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.djg.dmall_api.controller"})
 public class Dmall_apiApplication {
 
 	public static void main(String[] args) {
